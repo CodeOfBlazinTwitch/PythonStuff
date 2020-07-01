5c1e318a9b68b2da9212ec37550f40f487da8da8
BlazinTwitch <67522504+BlazinTwitch@users.noreply.github.com>
2020-06-30 18:49:46 -0700

    This is the Fibbonaci Sequence.
    Thank You!

---
+++ FibbonacciSequence.py
@@ -0,0 +1,18 @@
+i = int(input("How Many Numbers? "))
+
+a = 1
+b = 1
+counter = 2
+print("1")
+print("1")
+
+while counter < i:
+   
+    if counter != 2:
+        a = c
+    c = a + b
+   
+    print(c)
+    b = a
+   
+    counter += 1
