5e5071ba11591f81e0e69524048fb869087aec58
BlazinTwitch <blazintwitch@gmail.com>
2024-11-01 19:36:17 -0700

    Added new What the flip comment

--- PandasTesting.py
+++ PandasTesting.py
@@ -12,4 +12,4 @@ data2 = pd.Series([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
 print("describe: \n", data2.describe())
 print("agg: \n", data2.agg(["mean", "std", "min", "max"]))
 
-hi
+What the flip
